21393dfeb8639154ee0a13e65b0e7fae0ae5104f
Conanas <tmhaigh@gmail.com>
2020-10-16 11:38:18 +1100

    added require api-routes

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 require("./routes/html-routes")(app);
+require("./routes/api-routes")(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
