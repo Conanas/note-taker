ae1626daa6923dc9c7df9b55ad0119daf04f03e4
Conanas <tmhaigh@gmail.com>
2020-10-17 10:20:55 +1100

    added path module + conifg for static js+css files

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "/public")));
 
 require("./routes/html-routes")(app);
 require("./routes/api-routes")(app);
